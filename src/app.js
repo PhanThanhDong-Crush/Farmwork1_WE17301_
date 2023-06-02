9278ff99238954da45de5e805e4025a7c16f5a29
PhanThanhDong-Crush <97138310+PhanThanhDong-Crush@users.noreply.github.com>
2023-06-02 09:57:38 +0700

    feat: api add product refs: FW1-55

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ import ProductRouter from "./routes/product";
 
 const app = express();
 
-app.use(express.json());
-app.use("/api", ProductRouter);
-app.use(cors());
+app.use( express.json() );
+app.use( "/api", ProductRouter );
+app.use( cors() );
 
-mongoose.connect(`mongodb://127.0.0.1:27017/web208`);
+mongoose.connect( `mongodb://127.0.0.1:27017/web208` );
 
 export const viteNodeApp = app;
--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,6 +1,8 @@
+import { ProductSchema } from "../Schema/product";
 import Product from "../models/product";
 
-export const getAll = async function (req, res) {
+export const getAllPro = async function ( req, res )
+{
   const {
     _sort = "createAt",
     _order = "asc",
@@ -12,33 +14,72 @@ export const getAll = async function (req, res) {
     page: _page,
     limit: _limit,
     sort: {
-      [_sort]: _order == "desc" ? -1 : 1,
+      [ _sort ]: _order == "desc" ? -1 : 1,
     },
   };
 
-  try {
-    const { docs, totalDocs, totalPages } = await Product.paginate({}, options);
-    if (docs.length === 0) {
-      return res.status(400).json({ message: "Không có sản phẩm nào" });
+  try
+  {
+    const { docs, totalDocs, totalPages } = await Product.paginate( {}, options );
+    if ( docs.length === 0 )
+    {
+      return res.status( 400 ).json( { message: "Không có sản phẩm nào" } );
     }
-    return res.status(200).json({ data: docs, totalDocs, totalPages });
-  } catch (error) {
-    return res.json({
+    return res.status( 200 ).json( { data: docs, totalDocs, totalPages } );
+  } catch ( error )
+  {
+    return res.json( {
       message: error.message,
-    });
+    } );
   }
 };
 
-export const get = async function (req, res) {
-  try {
-    const data = await Product.findOne({ _id: req.params.id });
-    if (!data) {
-      return res.status(400).json({ message: "Không có sản phẩm nào" });
+export const getOnePro = async function ( req, res )
+{
+  try
+  {
+    const data = await Product.findOne( { _id: req.params.id } );
+    if ( !data )
+    {
+      return res.status( 400 ).json( { message: "Không có sản phẩm nào" } );
     }
-    return res.json(data);
-  } catch (error) {
-    return res.json({
+    return res.json( data );
+  } catch ( error )
+  {
+    return res.json( {
       message: error.message,
-    });
+    } );
   }
 };
+
+export const addPro = async ( req, res ) =>
+{
+  try
+  {
+    const { error } = ProductSchema.validate( req.body );
+    if ( error )
+    {
+      return res.status( 400 ).json( {
+        message: error.details[ 0 ].message,
+      } );
+    }
+    const product = await Product.create( req.body );
+    if ( !product )
+    {
+      return res.json( {
+        message: "Không thêm sản phẩm",
+      } );
+    }
+
+    return res.json( {
+      message: "Thêm sản phẩm thành công",
+      data: product,
+    } );
+  } catch ( error )
+  {
+    return res.status( 400 ).json( {
+      message: error,
+    } );
+  }
+}
+
