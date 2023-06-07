ec411019687d0323bf3000e93a7bb428ed6bcde3
PhanThanhDong-Crush <dongptph28020@fpt.edu.vn>
2023-06-07 09:43:50 +0700

    feat: api tim theo name product refs: FW1-87

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addPro, getAllPro, getOnePro, new3Pro, removePro, updatePro } from "../controllers/product";
+import { addPro, getAllPro, getOnePro, getPro_Name, new3Pro, removePro, updatePro } from "../controllers/product";
 
 const router = express.Router();
 
 router.get("/products", getAllPro);
 router.get("/products/:id", getOnePro);
 router.get("/products-new", new3Pro);
+router.get("/products-name", getPro_Name);
 router.post("/products/add", addPro);
 router.put("/products/edit/:id", updatePro);
 router.delete("/products/:id", removePro);
